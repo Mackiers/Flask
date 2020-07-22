eee38ead5953e06c5622e6f009d036a95d4aff2b
Mackiers <67982622+Mackiers@users.noreply.github.com>
2020-07-22 16:25:17 +0100

    Updates to website have been made. Password reset emails are working.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+from flask_mail import Mail
 
 app = Flask(__name__,
             template_folder='Templates')  # Name of module, so flask knows where to look for templates, static files etc...
@@ -12,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_DEFAULT_SENDER'] = ('Password Reset', 'noreply@vlogit.com')
+mail = Mail(app)
 
 from flaskblog import routes
